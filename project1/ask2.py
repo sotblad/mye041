9c903b2f259a2b36f0af1f88c21fdd1d8bcbebab
Sotiris Blad <sotblad@monetaryunit.org>
2021-03-10 17:48:10 +0200

    Update ask2.py

--- project1/ask2.py
+++ project1/ask2.py
@@ -1,42 +1,52 @@
 string = ""
 with open('R_sorted.tsv') as R, open('S_sorted.tsv') as S:
-    R_lines = R.read().split('\n')[:-1]
-    S_lines = S.read().split('\n')[:-1]
-    pointer = 0
-    mark = -1 #not set
+    R_line = R.readline().rstrip()
+    S_line = S.readline().rstrip()
     tmp = None
     
-    for i, line in enumerate(R_lines):
-        R = line.split("\t");
-        
-        
-        if(R[0] < S_lines[pointer].split("\t")[0]):
-            print(R[0], R[1])
+    while R_line:
+        while(R_line.split('\t')[0] < S_line.split('\t')[0]):
+            if(tmp != R_line):
+                print(R_line.split('\t')[0], R_line.split('\t')[1])
+            tmp = R_line
+            R_line = R.readline().rstrip()
             pass
-        while(R[0] > S_lines[pointer].split("\t")[0]):
-            print(S_lines[pointer].split("\t")[0], S_lines[pointer].split("\t")[1])
-            if(R == S_lines[pointer+1].split("\t")):
-                pointer += 1
-            if(S_lines[pointer].split("\t")[0] != S_lines[pointer+1].split("\t")[0]):
-                if(R[0] <= S_lines[pointer].split("\t")[0]):
-                    print(R[0],R[1])
-            pointer += 1
+    
+        while(R_line.split('\t')[0] > S_line.split('\t')[0]):
+            if(tmp != S_line):
+                print(S_line.split('\t')[0], S_line.split('\t')[1])
+            tmp = S_line
+            S_line = S.readline().rstrip()
+    
+        if(R_line.split('\t')[0] == S_line.split('\t')[0]):
+            while(R_line.split('\t')[0] == S_line.split('\t')[0]):
+                if(R_line.split('\t')[1] < S_line.split('\t')[1]):
+                    if(tmp != R_line):
+                        print(R_line.split('\t')[0], R_line.split('\t')[1])
+                    tmp = R_line
+                    R_line = R.readline().rstrip()
+                else:
+                    if(tmp != S_line):
+                        print(S_line.split('\t')[0], S_line.split('\t')[1])
+                    tmp = S_line
+                    S_line = S.readline().rstrip()
+            if(R_line.split('\t')[0] < S_line.split('\t')[0]):
+                if(tmp != R_line and R_line != ""):
+                    print(R_line.split('\t')[0], R_line.split('\t')[1])
+                tmp = R_line
         
-        if(R[0] == S_lines[pointer].split("\t")[0]):
-            while(R[0] == S_lines[pointer].split("\t")[0]):
-                if(R[1] <= S_lines[pointer].split("\t")[1]):
-                    print(R[0], R[1])
-                    break
-                if(R[1] > S_lines[pointer].split("\t")[1]):
-                    string = string + R[0] + "\t" + R[1] + "\t" + S_lines[pointer].split("\t")[1] + "\n"
-                    print(R[0], S_lines[pointer].split("\t")[1])
-                    
-                    if(R == S_lines[pointer+1].split("\t")):
-                        pointer += 1
-                    if(S_lines[pointer].split("\t")[0] != S_lines[pointer+1].split("\t")[0]):
-                        print(R[0],R[1])
-                    pointer += 1
-
+        if(R_line == ""): #print rest of S
+            while(S_line):
+                if(tmp != S_line):
+                    print(S_line.split('\t')[0], S_line.split('\t')[1])
+                    tmp = S_line
+                    S_line = S.readline().rstrip()
+        elif(S_line == ""): #print rest of R
+            while(R_line):
+                if(tmp != R_line):
+                    print(R_line.split('\t')[0], R_line.split('\t')[1])
+                    tmp = R_line
+                    R_line = R.readline().rstrip()
 
 f = open("RunionS.tsv", "w")
 f.write(string)
