e34305601df3c41649ce2d32a83107c912b8c275
Sotiris Blad <sotblad@monetaryunit.org>
2021-03-12 03:06:32 +0200

    Update ask2.py

--- project1/ask2.py
+++ project1/ask2.py
@@ -5,14 +5,14 @@ with open('R_sorted.tsv') as R, open('S_sorted.tsv') as S:
     tmp = None
     
     while R_line:
-        while(R_line.split('\t')[0] < S_line.split('\t')[0]):
+        while(R_line != "" and R_line.split('\t')[0] < S_line.split('\t')[0]):
             if(tmp != R_line):
                 print(R_line.split('\t')[0], R_line.split('\t')[1])
             tmp = R_line
             R_line = R.readline().rstrip()
             pass
     
-        while(R_line.split('\t')[0] > S_line.split('\t')[0]):
+        while(S_line != "" and R_line.split('\t')[0] > S_line.split('\t')[0]):
             if(tmp != S_line):
                 print(S_line.split('\t')[0], S_line.split('\t')[1])
             tmp = S_line
@@ -39,14 +39,14 @@ with open('R_sorted.tsv') as R, open('S_sorted.tsv') as S:
             while(S_line):
                 if(tmp != S_line):
                     print(S_line.split('\t')[0], S_line.split('\t')[1])
-                    tmp = S_line
-                    S_line = S.readline().rstrip()
+                tmp = S_line
+                S_line = S.readline().rstrip()
         elif(S_line == ""): #print rest of R
             while(R_line):
                 if(tmp != R_line):
                     print(R_line.split('\t')[0], R_line.split('\t')[1])
-                    tmp = R_line
-                    R_line = R.readline().rstrip()
+                tmp = R_line
+                R_line = R.readline().rstrip()
 
 f = open("RunionS.tsv", "w")
 f.write(string)
